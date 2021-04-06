7797b7b3850987502f11d19ee4cf64363268d898
bdavs3 <bdavs3000@gmail.com>
2021-04-06 10:08:33 -0700

    Scaffold API call structure
    • Create a method for making client requests
    • Create response.go within the facebook package. Populate it with types used in unmarshalling data retrieved from the Ad Library

--- facebook/client.go
+++ facebook/client.go
@@ -1,6 +1,10 @@
 package facebook
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -8,6 +12,7 @@ import (
 const (
 	adLibraryURL = "https://graph.facebook.com/v10.0/ads_archive"
 	timeout      = 5 * time.Second
+	accessToken  = "EAACFomGEN60BABU0dlxZA4ZChhlx4GfSGXRAIW6S2ZBXD0J6kjZACZB0ZCj2adNcyqthZAly5nn8W1UjR9EZBzHYgGZCrfCuW5b6q1fxJ6GZC6jA1cDk26UdswvK5lz6QCo0x2XcMHrXToeQZBgzGQmc9CiZCUBYaLFskV5kTzsUmZABZCOjqL35fn6beZAGvW0JfoECw5YdZAA1snlPNXWZA3o7RyP48ic4l6oLFMzEaeBG9iWAS51GCoIfEXiOF5WfMzCQmIHUZD"
 )
 
 type Client struct {
@@ -24,4 +29,38 @@ func NewClient() *Client {
 
 func (c *Client) String() string {
 	return c.BaseURL
+}
+
+func (c *Client) MakeRequest(method string, requestBody io.Reader) (*Response, error) {
+	req, err := http.NewRequest(
+		method,
+		c.BaseURL,
+		requestBody,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s\n%s", http.StatusText(resp.StatusCode), body)
+	}
+
+	var response *Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
 }
---
+++ facebook/response.go
@@ -0,0 +1,35 @@
+package facebook
+
+type Response struct {
+	ID                        int                    `json:"id"`
+	AdCreationTime            string                 `json:"ad_creation_time,omitempty"`
+	AdCreativeBody            string                 `json:"ad_creative_body,omitempty"`
+	AdCreativeLinkCaption     string                 `json:"ad_creative_link_caption,omitempty"`
+	AdCreativeLinkDescription string                 `json:"ad_creative_link_description,omitempty"`
+	AdCreativeLinkTitle       string                 `json:"ad_creative_link_title,omitempty"`
+	AdDeliveryStartTime       string                 `json:"ad_delivery_start_time,omitempty"`
+	AdDeliveryStopTime        string                 `json:"ad_delivery_stop_time,omitempty"`
+	AdSnapshotURL             string                 `json:"ad_snapshot_url,omitempty"`
+	Currency                  string                 `json:"currency,omitempty"`
+	DemographicDistribution   []AudienceDistribution `json:"demographic_distribution,omitempty"`
+	FundingEntity             string                 `json:"funding_entity,omitempty"`
+	Impressions               InsightsRange          `json:"impressions,omitempty"`
+	PageID                    string                 `json:"page_id,omitempty"`
+	PageName                  string                 `json:"page_name,omitempty"`
+	PotentialReach            InsightsRange          `json:"potential_reach,omitempty"`
+	PublisherPlatforms        []string               `json:"publisher_platforms,omitempty"`
+	RegionDistribution        []AudienceDistribution `json:"region_distribution,omitempty"`
+	Spend                     InsightsRange          `json:"spend,omitempty"`
+}
+
+type AudienceDistribution struct {
+	Age        string
+	Gender     string
+	Percentage string
+	Region     string
+}
+
+type InsightsRange struct {
+	LowerBound string
+	UpperBound string
+}
