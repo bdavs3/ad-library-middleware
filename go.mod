d4e57a185e0a82ebe5c4597396f7a9b64b4c7208
bdavs3 <bdavs3000@gmail.com>
2021-04-12 12:52:38 -0700

    Use Go Bigquery package to scaffold a basic RunQuery method

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ad-library-middleware
 
 go 1.16
 
-require github.com/google/go-querystring v1.1.0 // indirect
+require (
+	cloud.google.com/go/bigquery v1.17.0
+	google.golang.org/api v0.43.0
+)
--- google/bigquery.go
+++ google/bigquery.go
@@ -1,1 +1,49 @@
 package google
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
+)
+
+type Connection struct {
+	ctx    context.Context
+	client *bigquery.Client
+}
+
+func NewConnection(project string) (*Connection, error) {
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, project)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Connection{
+		ctx:    ctx,
+		client: client,
+	}, nil
+}
+
+func (conn *Connection) RunQuery(body string) error {
+	it, err := conn.client.Query(body).Read(conn.ctx)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Print any output to an audit log
+	for {
+		var values []bigquery.Value
+		err := it.Next(&values)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Println(values)
+	}
+
+	return nil
+}
